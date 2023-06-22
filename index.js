5fc2e76ceca1cf7b053d993b1e505af1778301e8
Bill <wjquayle@gmail.com>
2023-06-22 16:32:03 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b){//1
+    return a + b;
+}
+
+function subtract(a, b){//2
+    return a - b;
+}
+
+function multiply(a, b){//3
+    return a * b;
+}
+
+function divide(a, b){//4
+    return a / b;
+}
+function increment(n){
+    n++;
+    return (n);
+}
+
+function decrement(n){
+    n--;
+    return (n);
+}
+
+function makeInt(n){
+    return (parseInt(n, 10));
+
+}
+
+function preserveDecimal(n){
+    return(parseFloat(n));
+    
+}
